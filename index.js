df7f4f714037b14027b9018cbdf5b45f8a34d934
Vladimir <vladkodoktorov@gmail.com>
2020-10-07 09:18:44 +0100

    update dependency

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs-node');
 const loadCSV = require('./load-csv');
 
+function knn(features, labels, predictionPoint, k){
+     return features
+     .sub(predictionPoint)
+     .pow(2)
+     .sum(1)
+     .pow(0.5)
+     .expandDims(1) // expand dimension due to different shapes
+     .concat(labels, 1) //join tensors
+     .unstack() //create js array of tensors from tensor
+     .sort((a, b) => a.get(0) > b.get(0) ? 1 : -1)
+     .slice(0, k)
+     .reduce((acc, pair) => acc + pair.get(1), 0) / k     //we are using regression so getting an average house price
+
+}
+
 let {features, labels, testFeatures, testLabels} = loadCSV('kc_house_data.csv', {
      shuffle: true,
      splitTest: 10,
@@ -9,5 +24,9 @@ let {features, labels, testFeatures, testLabels} = loadCSV('kc_house_data.csv', 
      labelColumns: ['price']
 });
 
-console.log(testFeatures);
-console.log(testLabels);
+//converting arrays to tensors
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10 );
+console.log('Guess', result, testLabels[0][0])
